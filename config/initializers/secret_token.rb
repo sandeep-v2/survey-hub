083d4c69e783fe15515844296e8ddc7d1127eee6
sandeep patle <sandeep.patle@in.v2solutions.com>
2013-09-10 12:10:10 +0530

    Task-1: 1. Setup the ruby on rails enviroment for project. 2. Added devise with User model for  authentication solution

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Survey::Application.config.secret_token = '44ff6083144f8581821c3cba1355898646a82c1968d2ef80d05673fa003b378742357717ea68dfb2b5221d2a922c65b599bd859f1d1d85a1156cf5272f81f104'

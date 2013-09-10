a251de175585c7ba91708dd2839c322005e7aa73
sandeep patle <sandeep.patle@in.v2solutions.com>
2013-09-10 19:47:38 +0530

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyHub::Application.config.secret_token = '0c0fa508fe201e5956db7fc16d1eb3c4342c3a347b5981b0776ceaf12e3f9d5faa1adc04ab861abf3b9226af7b039d5da12ecbc317b87122e93d2ca2bb1721a0'
